47374efb90fb50aa4ad946a3170b5a7e627298be
MD ASHIF ANSARI <mdashifansari454@gmail.com>
2025-06-22 17:32:50 +0530

    Update singlyLinkedList_Creation & print.c

--- singlyLinkedList_Creation & print.c
+++ singlyLinkedList_Creation & print.c
@@ -35,27 +35,59 @@ struct Node *createList(int n)
     int data;
     printf("Enter the value for node 1 :\t");
     scanf("%d", &data);
-    system("cls");
+
     struct Node *head = createNode(data);
     struct Node *ptr = head;
     for (int i = 2; i <= n; i++)
     {
         printf("Enter the value for node %d :\t", i);
         scanf("%d", &data);
-        system("cls");
+
         // ptr->next = createNode(data);
         ptr = ptr->next = createNode(data);
     }
     return head;
 }
 
+struct Node *insertBeg(struct Node *head)
+{
+    int data;
+
+    printf("\nEnter value for Node which is Insert at Beginning :\t");
+    scanf("%d", &data);
+    struct Node *ptr = createNode(data);
+    ptr->next = head;
+    head = ptr;
+    return head;
+}
+
+struct Node *insertEnd(struct Node *head)
+{
+    struct Node *ptr = head;
+    int data;
+    while (ptr->next != NULL)
+    {
+        ptr = ptr->next;
+    }
+    printf("\nEnter value for Node Which is Insert at End :\t");
+    scanf("%d", &data);
+    struct Node *tmp = createNode(data);
+    ptr->next = tmp;
+    return head;
+}
+
 int main()
 {
     int n;
+
     printf("Enter a length of Linked List : \t");
     scanf("%d", &n);
-    system("cls");
+
     struct Node *head = createList(n);
+    show(head);
+    head = insertBeg(head);
+    show(head);
+    head = insertEnd(head);
     show(head);
     return 0;
 }
