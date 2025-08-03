b0c7761a662a9445a6c830e339ca4b0f1b0c688f
MD ASHIF ANSARI <mdashifansari454@gmail.com>
2025-08-03 16:41:06 +0530

    Update linkedListUsingClass.cpp

--- linkedListUsingClass.cpp
+++ linkedListUsingClass.cpp
@@ -30,77 +30,84 @@ public:
               return new Node(value);
        }
 
-       void insertAtEnd(int value)
-       {
-              Node *newNode = createNode(value);
-              if (head == nullptr)
-              {
-                     head = newNode;
-                     return;
-              }
-              Node *temp = head;
-              while (temp->next != nullptr)
-                     temp = temp->next;
-              temp->next = newNode;
-       }
+       void insertAtEnd(int value);
 
-       void insertAtBeginning(int value)
-       {
-              Node *newnode = createNode(value);
-              newnode->next = head;
-              head = newnode;
-       }
+       void insertAtBeginning(int value);
 
-       void createList(int n)
-       {
-              int value;
-              char ch;
-              for (int i = 1; i <= n; i++)
-              {
-                     cout << "Enter Node " << i << " Data/Value  : ";
-                     cin >> value;
-                     cout << "Insert this data at the beginning or end of the linked list.\n Press 'B' for beginning and Press 'E' for end : ";
-                     cin >> ch;
-                     if (ch == 'b' || ch == 'B')
-                     {
-                            insertAtBeginning(value);
-                            cout << value << " is Successfully inserted at Beginning of Linked List.\n";
-                     }
-                     else if (ch == 'e' || ch == 'E')
-                     {
-                            insertAtEnd(value);
-                            cout << value << " is Successfully inserted at End of Linked list.\n";
-                     }
-                     else
-                            cout << "You pressed another key except 'B' and 'E' so " << value << " is not inserted ! \n";
-              }
-       }
+       void createList(int n);
 
-       void display()
-       {
-              if (head == nullptr)
-              {
-                     cout << "Linked List is Empty :";
-                     return;
-              }
-              Node *temp = head;
-              while (temp != nullptr)
-              {
-                     cout << temp->data << " -> ";
-                     temp = temp->next;
-              }
-              cout << "NULL\n";
-       }
+       void display();
 };
+
+void linkedList::insertAtBeginning(int value)
+{
+       Node *newnode = createNode(value);
+       newnode->next = head;
+       head = newnode;
+}
+
+void linkedList::insertAtEnd(int value)
+{
+       Node *newNode = createNode(value);
+       if (head == nullptr)
+       {
+              head = newNode;
+              return;
+       }
+       Node *temp = head;
+       while (temp->next != nullptr)
+              temp = temp->next;
+       temp->next = newNode;
+}
+
+void linkedList::display()
+{
+       if (head == nullptr)
+       {
+              cout << "Linked List is Empty :";
+              return;
+       }
+       Node *temp = head;
+       while (temp != nullptr)
+       {
+              cout << temp->data << " -> ";
+              temp = temp->next;
+       }
+       cout << "NULL\n";
+}
+void linkedList::createList(int n)
+{
+       int value;
+       char ch;
+       for (int i = 1; i <= n; i++)
+       {
+              cout << "Enter Node " << i << " Data/Value  : ";
+              cin >> value;
+              cout << "Insert this data at the beginning or end of the linked list.\n Press 'B' for beginning and Press 'E' for end : ";
+              cin >> ch;
+              if (ch == 'b' || ch == 'B')
+              {
+                     insertAtBeginning(value);
+                     cout << value << " is Successfully inserted at Beginning of Linked List.\n";
+              }
+              else if (ch == 'e' || ch == 'E')
+              {
+                     insertAtEnd(value);
+                     cout << value << " is Successfully inserted at End of Linked list.\n";
+              }
+              else
+                     cout << "You pressed another key except 'B' and 'E' so " << value << " is not inserted ! \n";
+       }
+}
 
 int main()
 {
        system("cls");
-       linkedList o1;
+       linkedList list;
        int n;
        cout << "Enter the Length of Linked List : ";
        cin >> n;
-       o1.createList(n);
-       o1.display();
+       list.createList(n);
+       list.display();
        return 0;
 }
